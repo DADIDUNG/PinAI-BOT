09141b097c18a64f0178a077790d1fcb390df19c
DANGGIA <116240457+DADIDUNG@users.noreply.github.com>
2025-02-16 14:47:36 +0700

    Update banner.js

--- config/banner.js
+++ config/banner.js
@@ -1,7 +1,7 @@
 function printBanner() {
   console.log("=========== PinAI BOT ===========");
-  console.log("GitHub: https://github.com/Galkurta");
-  console.log("Telegram: https://t.me/galkurtarchive");
+  console.log("GitHub: https://github.com/DADIDUNG");
+  console.log("Telegram: https://t.me/dadidng");
   console.log("=================================");
 }
 module.exports = printBanner;
